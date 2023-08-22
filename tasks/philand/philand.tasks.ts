7410d3401e4027e1136beaaf5135ff5802cd0d8a
tridetch <tridetch@gmail.com>
2023-08-23 01:04:51 +0400

    philand daily quests

--- tasks/philand/philand.tasks.ts
+++ tasks/philand/philand.tasks.ts
@@ -1,11 +1,31 @@
-import { ethers } from "ethers";
+import { Contract, ethers } from "ethers";
 import { task, types } from "hardhat/config";
 import { ChainId, getChainInfo } from "../../utils/ChainInfoUtils";
 import "../../utils/Util.tasks";
 import { delay, getAccounts, populateTxnParams } from "../../utils/Utils";
-import { log } from "@uniswap/smart-order-router";
 
-task("philandDailyQuest", "Pliland dailty quest")
+import axios from "axios";
+
+interface QuestResult {
+    message: string;
+    day: number;
+    expiresIn: number;
+    coupons: CouponInfo[];
+}
+
+interface CouponInfo {
+    logic: number;
+    coupon: Coupon;
+}
+
+interface Coupon {
+    r: string;
+    s: string;
+    v: string;
+}
+
+task("philandDailyQuest", "Philand dailty quest")
+    .addFlag("claim", "Only try to claim quests")
     .addParam("delay", "Add delay between operations", undefined, types.float, true)
     .addOptionalParam("startAccount", "Starting account index", undefined, types.string)
     .addOptionalParam("endAccount", "Ending account index", undefined, types.string)
@@ -24,27 +44,70 @@ task("philandDailyQuest", "Pliland dailty quest")
             return;
         }
 
+        const questContractAddress = "0xa4a057e817a220e4a9466e7877adbdb917a9d8d9";
+
+        const questContract = new Contract(questContractAddress, [
+            "function claimMaterialObject(uint32 eventId, uint16 logicId, (bytes32 r, bytes32 s, uint8 v) coupon, uint256 expiresIn)",
+        ]);
+
         const accounts = await getAccounts(taskArgs, hre.ethers.provider);
 
         for (const account of accounts) {
             try {
                 console.log(`\n#${accounts.indexOf(account)} Address: ${account.address}`);
 
-                console.log(`Making 8 transactions...`);
+                if (!taskArgs.claim) {
+                    console.log(`Making transactions...`);
 
-                for (let iteration = 1; iteration <= 8; iteration++) {
-                    var txParams = await populateTxnParams({ signer: account, chain: chainInfo });
-                    const dailyTx = await account.sendTransaction({
-                        to: account.address,
-                        value: ethers.constants.Zero,
-                        ...txParams,
-                    });
-                    console.log(`Txn ${iteration}: ${chainInfo.explorer}${dailyTx.hash}`);
+                    for (let iteration = 1; iteration <= 10; iteration++) {
+                        var txParams = await populateTxnParams({ signer: account, chain: chainInfo });
+                        const dailyTx = await account.sendTransaction({
+                            to: account.address,
+                            value: ethers.constants.Zero,
+                            ...txParams,
+                        });
+                        console.log(`Txn ${iteration}: ${chainInfo.explorer}${dailyTx.hash}`);
 
-                    await dailyTx.wait();
+                        await dailyTx.wait();
+                    }
+
+                    console.log(`Transactions sended successfully! Wait when they will be indexed.`);
+                    await delay(0.2)
                 }
 
-                console.log(`Transactions sended successfully!`);
+                const questResult: QuestResult = (
+                    await axios.get(
+                        `https://utils-api.phi.blue/v1/philand/dailyquest/verify?address=${account.address}`
+                    )
+                ).data;
+
+                // console.log(JSON.stringify(questResult));
+
+                for (const couponInfo of questResult.coupons) {
+                    try {
+                        var txParams = await populateTxnParams({ signer: account, chain: chainInfo });
+                        const claimTx = await questContract.connect(account).claimMaterialObject(
+                            questResult.day,
+                            couponInfo.logic,
+                            {
+                                r: couponInfo.coupon.r,
+                                s: couponInfo.coupon.s,
+                                v: couponInfo.coupon.v,
+                            },
+                            questResult.expiresIn,
+                            { ...txParams }
+                        );
+                        await claimTx.wait(5);
+
+                        console.log(
+                            `Claim quest ${couponInfo.logic} - tx ${chainInfo.explorer}${claimTx.hash}`
+                        );
+                    } catch (error) {
+                        console.log(`Claim quest ${couponInfo.logic} failed. Already claimed?`);
+                        // console.log(error);
+                    }
+                }
+                console.log(`Daily quests completed!`);
 
                 if (taskArgs.delay != undefined) {
                     await delay(taskArgs.delay);
