a59f9f440557e01c74986d038176b34fcd06ae53
tridetch <tridetch@gmail.com>
2023-08-29 17:12:38 +0400

    base summer

--- tasks/coinbase/base_mainnet_summer.tasks.ts
+++ tasks/coinbase/base_mainnet_summer.tasks.ts
@@ -1,8 +1,8 @@
-import { BigNumber, Contract, ethers, utils } from "ethers";
+import { Contract, ethers, utils } from "ethers";
 import { task, types } from "hardhat/config";
 import { ChainId, getChainInfo } from "../../utils/ChainInfoUtils";
 import "../../utils/Util.tasks";
-import { addDust, delay, getAccounts, populateTxnParams, waitForGasPrice } from "../../utils/Utils";
+import { delay, getAccounts, populateTxnParams } from "../../utils/Utils";
 
 task("baseMintOnchainSummerNFT", "Mint Onchain Summer NFT")
     .addParam("delay", "Add delay between operations", undefined, types.float, true)
@@ -342,6 +342,66 @@ task("baseMintOnchainSummerZorbNFT", "Mint Onchain Summer Zorb NFT")
             } catch (error) {
                 console.log(
                     `Error when process account #${accounts.indexOf(account)} Address: ${account.address}`
+                );
+                console.log(error);
+            }
+        }
+    });
+
+task("baseMintOnchainSummerClashMonNFT", "Mint Onchain Summer ClashMon NFT")
+    .addParam("delay", "Add delay between operations", undefined, types.float, true)
+    .addOptionalParam("startAccount", "Starting account index", undefined, types.string)
+    .addOptionalParam("endAccount", "Ending account index", undefined, types.string)
+    .addOptionalParam(
+        "accountIndex",
+        "Index of the account for which it will be executed",
+        undefined,
+        types.string
+    )
+    .setAction(async (taskArgs, hre) => {
+        const contractAddress = "0x75ed58e1d029853231a9e9825f0035e0449fbafa";
+        const currentNetwork = await hre.ethers.provider.getNetwork();
+        const chainInfo = getChainInfo(currentNetwork.chainId);
+
+        if (![ChainId.baseMainnet].includes(currentNetwork.chainId)) {
+            console.log("Task available only at Base mainnet network!");
+            return;
+        }
+
+        const accounts = await getAccounts(taskArgs, hre.ethers.provider);
+
+        const mintContract = new Contract(
+            contractAddress,
+            [
+                "function claim(address, uint256, address, uint256, (bytes32[],uint256,uint256,address), bytes) payable",
+            ],
+            hre.ethers.provider
+        );
+
+        for (const account of accounts) {
+            try {
+                console.log(`\n#${accounts.indexOf(account)} Address: ${account.address}`);
+
+                const calldata = `0x84bb1e42000000000000000000000000${account.address.slice(
+                    2
+                )}0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000160000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000d93af387`;
+
+                const txParams = populateTxnParams({ signer: account, chain: chainInfo });
+
+                const mintTx = await account.sendTransaction({
+                    to: mintContract.address,
+                    data: calldata,
+                    ...txParams,
+                });
+
+                console.log(`Mint txn: ${chainInfo.explorer}${mintTx.hash}`);
+
+                if (taskArgs.delay != undefined) {
+                    await delay(taskArgs.delay);
+                }
+            } catch (error) {
+                console.log(
+                    `\nError when process account #${accounts.indexOf(account)} Address: ${account.address}`
                 );
                 console.log(error);
             }
