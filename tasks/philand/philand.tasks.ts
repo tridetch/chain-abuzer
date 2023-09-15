902e0a73310ec68f7c9a029039157206f4cffa39
tridetch <tridetch@gmail.com>
2023-09-15 12:43:49 +0400

    philand autiClaimer, check XP

--- tasks/philand/philand.tasks.ts
+++ tasks/philand/philand.tasks.ts
@@ -21,8 +21,56 @@ interface CouponInfo {
 interface Coupon {
     r: string;
     s: string;
-    v: string;
+    v: number;
 }
+
+task("philandCheckXp", "Philand check XP")
+    .addFlag("claim", "Only try to claim quests")
+    .addParam("delay", "Add delay between operations", undefined, types.float, true)
+    .addOptionalParam("startAccount", "Starting account index", undefined, types.string)
+    .addOptionalParam("endAccount", "Ending account index", undefined, types.string)
+    .addOptionalParam(
+        "accountIndex",
+        "Index of the account for which it will be executed",
+        undefined,
+        types.string
+    )
+    .setAction(async (taskArgs, hre) => {
+        const currentNetwork = await hre.ethers.provider.getNetwork();
+        const chainInfo = getChainInfo(currentNetwork.chainId);
+
+        if (![ChainId.poligonMainnet].includes(currentNetwork.chainId)) {
+            console.log("Task available only at Polygon mainnet network!");
+            return;
+        }
+
+        const accounts = await getAccounts(taskArgs, hre.ethers.provider);
+
+        for (const account of accounts) {
+            try {
+                console.log(`\n#${accounts.indexOf(account)} Address: ${account.address}`);
+
+                let response: AxiosResponse | any = undefined;
+
+                try {
+                    response = await axios.get(
+                        `https://utils-api.phi.blue/v1/philand/account/achieved_quest?address=${account.address}`
+                    );
+                    console.log(`Earned ${response.data.expGain} XP`);
+                } catch (error: any) {
+                    console.log(`Error ${error.response.status} ${error.response.data.result}`);
+                    continue;
+                }
+
+                if (taskArgs.delay != undefined) {
+                    await delay(taskArgs.delay);
+                }
+            } catch (error) {
+                console.log(`Error when process account`);
+                console.log(error);
+            }
+        }
+    });
 
 task("philandDailyQuest", "Philand dailty quest")
     .addFlag("claim", "Only try to claim quests")
@@ -113,6 +161,167 @@ task("philandDailyQuest", "Philand dailty quest")
                     }
                 }
                 console.log(`Daily quests completed!`);
+
+                if (taskArgs.delay != undefined) {
+                    await delay(taskArgs.delay);
+                }
+            } catch (error) {
+                console.log(`Error when process account`);
+                console.log(error);
+            }
+        }
+    });
+
+task("philandQuestAutoClaimer", "Philand claim all awailable quests")
+    .addParam("delay", "Add delay between operations", undefined, types.float, true)
+    .addFlag("skipCheck", "Skip check for new awailable quests")
+    .addOptionalParam("startAccount", "Starting account index", undefined, types.string)
+    .addOptionalParam("endAccount", "Ending account index", undefined, types.string)
+    .addOptionalParam(
+        "accountIndex",
+        "Index of the account for which it will be executed",
+        undefined,
+        types.string
+    )
+    .setAction(async (taskArgs, hre) => {
+        const currentNetwork = await hre.ethers.provider.getNetwork();
+        const chainInfo = getChainInfo(currentNetwork.chainId);
+
+        if (![ChainId.poligonMainnet].includes(currentNetwork.chainId)) {
+            console.log("Task available only at Poligon mainnet network!");
+            return;
+        }
+
+        interface CheckResponse {
+            address: string;
+            result: number[];
+        }
+
+        interface Quest {
+            TokenId: number;
+            Condition: string;
+            Value: number;
+            Counter: number;
+        }
+
+        interface ProgressResponse {
+            result: Quest[];
+        }
+
+        const questClaimContractAddress = "0x754e78bc0f7b487d304552810a5254497084970c";
+
+        const questClaimContract = new Contract(questClaimContractAddress, [
+            "function claimQuestObject(address contractAddress, uint256 tokenId, string condition, (bytes32 r, bytes32 s, uint8 v) coupon)",
+        ]);
+
+        const accounts = await getAccounts(taskArgs, hre.ethers.provider);
+
+        if (!taskArgs.skipCheck) {
+            console.log(`Update quests status. The process will take about two minutes...`);
+            for (const account of accounts) {
+                try {
+                    await axios.post(
+                        `https://utils-api.phi.blue/v1/philand/condition/check?address=${account.address}`
+                    );
+                    await new Promise((r) => setTimeout(r, 100));
+                } catch (error: any) {
+                    console.log(
+                        `#${accounts.indexOf(account)} Address: ${account.address} ERROR ${
+                            error.response.status
+                        } ${error.response.data.result}`
+                    );
+                }
+            }
+            await delay(2);
+        }
+
+        for (const account of accounts) {
+            try {
+                console.log(`\n#${accounts.indexOf(account)} Address: ${account.address}`);
+
+                const allAccountQuestsId: number[] = (
+                    await axios.get(
+                        `https://utils-api.phi.blue/v1/philand/condition/check?address=${account.address}`
+                    )
+                ).data.result;
+                // console.log("allAccountQuestsId");
+                // console.log(allAccountQuestsId);
+
+                const completedQuestsId: number[] = (
+                    await axios.get(
+                        `https://utils-api.phi.blue/v1/philand/account/achieved_quest?address=${account.address}`
+                    )
+                ).data.result;
+                // console.log("completedQuestsId");
+                // console.log(completedQuestsId);
+
+                const unclaimedQuestsId: number[] = allAccountQuestsId.filter(
+                    (id) => !completedQuestsId.includes(id)
+                );
+                // console.log("unclaimedQuestsId");
+                // console.log(unclaimedQuestsId);
+
+                const progressResponse: ProgressResponse = (
+                    await axios.get(
+                        `https://utils-api.phi.blue/v1/philand/condition/progress?address=${account.address}`
+                    )
+                ).data;
+                // console.log("progressResponse");
+                // console.log(progressResponse);
+
+                console.log(`${unclaimedQuestsId.length} unclaimed quests`);
+                if (unclaimedQuestsId.length == 0) continue;
+
+                for (const questId of unclaimedQuestsId) {
+                    try {
+                        let quest = progressResponse.result.find((quest) => quest.TokenId == questId);
+                        if (!quest) continue;
+
+                        // console.log(`Claim quest`);
+                        // console.log(quest);
+
+                        let coupon: Coupon = (
+                            await axios.get(
+                                `https://object-api.phi.blue/v1/quest_objects?address=${
+                                    account.address
+                                }&condition=${quest.Condition}&value=${quest.Value.toString().replace(
+                                    ".",
+                                    "p"
+                                )}`
+                            )
+                        ).data.coupon;
+
+                        var txParams = await populateTxnParams({ signer: account, chain: chainInfo });
+                        const claimTx = await questClaimContract.connect(account).claimQuestObject(
+                            "0x3D8C06e65ebf06A9d40F313a35353be06BD46038",
+                            quest.TokenId,
+                            `${quest.Condition}${quest.Value.toString().replace(".", "p")}`,
+                            {
+                                r: coupon.r,
+                                s: coupon.s,
+                                v: coupon.v,
+                            },
+                            { ...txParams }
+                        );
+                        console.log(
+                            `Quest claimed ${quest.Condition} ${quest.Value} - tx ${chainInfo.explorer}${claimTx.hash}`
+                        );
+                        await claimTx.wait();
+                    } catch (error) {
+                        console.log(`Claim quest failed`);
+                        console.log(error);
+                    }
+                }
+                let response: AxiosResponse | any = undefined;
+
+                try {
+                    response = await axios.get(
+                        `https://utils-api.phi.blue/v1/philand/account/achieved_quest?address=${account.address}`
+                    );
+                } catch (error: any) {
+                }
+
+                console.log(`All quests claimed! Current progress ${response.data.expGain} XP.`);
 
                 if (taskArgs.delay != undefined) {
                     await delay(taskArgs.delay);
